a11b4556d2075f73f4f142cf4481b6d4e3a76b4c
Masive <jozollo@gmail.com>
2021-06-20 09:18:38 +0200

    dodaje klase bank account

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        BankAccount Super = new BankAccount();
+        Super.transfer(1000);
+        Super.withdraw(700);
+        Super.getAccountBalance();
+        Super.withdraw(3000);
+    }
+}
